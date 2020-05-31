2041f0612d5064602733d4c5f4e1d06a042460ad
Negar <ngrcode@gmail.com>
2020-05-31 22:19:01 +0430

    from local

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,40 +1,70 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Badge from '@material-ui/core/Badge';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+const StyledBadge = withStyles((theme) => ({
+    badge: {
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: '$ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}))(Badge);
+
+const SmallAvatar = withStyles((theme) => ({
+    root: {
+        width: 22,
+        height: 22,
+        border: `2px solid ${theme.palette.background.paper}`,
+    },
+}))(Avatar);
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        // marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 2,
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
     },
 }));
 
-export default function ButtonAppBar() {
+export default function BadgeAvatars() {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-                    </Typography>
-                    <Button color="inherit" >Login</Button>
-                </Toolbar>
-            </AppBar>
+            <StyledBadge
+                overlap="circle"
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                variant="dot"
+            >
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </StyledBadge>
+
         </div>
     );
 }
